89560d77716692e18b5a43c85e9f2b3a7873104e
Jeffrey Marks <JKDMarks@gmail.com>
2019-10-25 15:38:54 -0400

    My face

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ function App() {
   return (
     <div className="App">
       <div className="header clearfix" style={{backgroundColor: "darkgrey", height: "15%"}}>
-        <div style={{float: "left", width: "50%"}}>
-          <h1 className="green-shadow" style={{textAlign: "center"}}>Jeffrey Marks</h1>
+        <div className="clearfix" style={{float: "left", width: "35%"}}>
+          <h1 style={{textAlign: "center", fontFamily: "Signika"}}>Jeffrey Marks</h1>
+
+          <img style={{borderRadius: "50%", height: "250px"}} src="https://avatars0.githubusercontent.com/u/48098502" alt="my face"/>
         </div>
 
         <div style={{float: "right", width: "50%"}}>
