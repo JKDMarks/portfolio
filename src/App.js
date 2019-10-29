0722930542fb7a4ad74c0664c61223ff67e7de17
Jeffrey Marks <JKDMarks@gmail.com>
2019-10-28 20:59:31 -0400

    Top container

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Menu } from 'semantic-ui-react'
+import { Grid, Segment, Popup, Icon } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 import './App.css'
 
@@ -10,32 +10,52 @@ function App() {
 
   return (
     <div className='App'>
-      <Menu size={'massive'}>
-        <Menu.Item header
-          content={'Jeffrey Marks'}
-        />
+      <Segment className='gray-container'>
+        <Grid centered>
+          <Grid.Column width={4} textAlign='center'>
+            <h1 className='title-text'>Jeffrey Marks</h1>
+            <img src='./jeff.jpg' id='face-pic' alt='my face'/>
+          </Grid.Column>
 
-        <Menu.Item
-          name={'about'}
-          content={'About'}
-          onClick={handleMenuItemClick}
-          active={activeTab === 'about'}
-        />
+          <Grid.Column width={1} />
 
-        <Menu.Item
-          name={'projects'}
-          content={'Projects'}
-          onClick={handleMenuItemClick}
-          active={activeTab === 'projects'}
-        />
+          <Grid.Column width={7} id='blurb'>
+            <p>Hi, I'm Jeff! My primary hat is that of a Full-Stack Software Engineer. I have extensive experience in JavaScript, React, Ruby, Ruby on Rails and much more.</p>
 
-        <Menu.Item
-          name={'resume'}
-          content={'Resume'}
-          onClick={handleMenuItemClick}
-          active={activeTab === 'resume'}
-        />
-      </Menu>
+            <br/>
+
+            <p style={{textAlign: 'center'}}>
+              <a href='https://github.com/jkdmarks'>
+                <Icon name='github square' size='big'/>
+              </a>
+
+              <a href='https://www.linkedin.com/in/jkdmarks/'>
+                <Icon name='linkedin' size='big'/>
+              </a>
+
+              <a href='https://medium.com/@jkdmarks'>
+                <Icon name='medium' size='big'/>
+              </a>
+            </p>
+          </Grid.Column>
+        </Grid>
+      </Segment>
+
+      <Segment className='gray-container'>
+        <h1 className='title-text' style={{textAlign: 'center'}}>Projects</h1>
+
+        <Grid columns={2} centered>
+          <Grid.Row>
+            <Grid.Column>hi</Grid.Column>
+            <Grid.Column>hi</Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row>
+            <Grid.Column>hi</Grid.Column>
+            <Grid.Column>hi</Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
     </div>
   )
 }
