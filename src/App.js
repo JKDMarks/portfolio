e67ec74a460f7a42717de848946ece0d8c1e7a37
Jeffrey Marks <JKDMarks@gmail.com>
2019-10-28 20:00:24 -0400

    With Menu

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Menu } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
 import './App.css'
 
 function App() {
+  const [activeTab, setActiveTab] = useState('about')
+
+  const handleMenuItemClick = (e, {name}) => setActiveTab(name)
+
   return (
-    <div className="App">
-      <div className="header clearfix" style={{backgroundColor: "darkgrey", height: "15%"}}>
-        <div className="clearfix" style={{float: "left", width: "35%"}}>
-          <h1 style={{textAlign: "center", fontFamily: "Signika"}}>Jeffrey Marks</h1>
+    <div className='App'>
+      <Menu size={'massive'}>
+        <Menu.Item header
+          content={'Jeffrey Marks'}
+        />
 
-          <img style={{borderRadius: "50%", height: "250px"}} src="https://avatars0.githubusercontent.com/u/48098502" alt="my face"/>
-        </div>
+        <Menu.Item
+          name={'about'}
+          content={'About'}
+          onClick={handleMenuItemClick}
+          active={activeTab === 'about'}
+        />
 
-        <div style={{float: "right", width: "50%"}}>
-          hi
-        </div>
-      </div>
+        <Menu.Item
+          name={'projects'}
+          content={'Projects'}
+          onClick={handleMenuItemClick}
+          active={activeTab === 'projects'}
+        />
+
+        <Menu.Item
+          name={'resume'}
+          content={'Resume'}
+          onClick={handleMenuItemClick}
+          active={activeTab === 'resume'}
+        />
+      </Menu>
     </div>
   )
 }
