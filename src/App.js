eb6b173d2b99dc38b14abbe2626033e0fbf66150
Jeffrey Marks <JKDMarks@gmail.com>
2019-10-28 22:46:11 -0400

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Grid, Segment, Icon } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 import './App.css'
