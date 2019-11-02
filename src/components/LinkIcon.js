d4c3c6287aba1ded096c2a1897e2df8966b9a0d9
Jeffrey Marks <JKDMarks@gmail.com>
2019-11-02 18:22:28 -0400

    LinkIcons open in new tab and cleanup manifest.json

--- src/components/LinkIcon.js
+++ src/components/LinkIcon.js
@@ -3,7 +3,7 @@ import { Icon } from 'semantic-ui-react'
 
 function LinkIcon({ url, iconName }) {
   return (
-    <a href={url}>
+    <a href={url} target='_blank'>
       <Icon name={iconName} size='big'/>
     </a>
   )
