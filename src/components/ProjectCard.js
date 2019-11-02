11c79dcd940e1c8e8aa2bd34a79523071e596e65
Jeffrey Marks <JKDMarks@gmail.com>
2019-11-02 18:39:09 -0400

    ProjectCard links target='_blank'

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -16,7 +16,7 @@ function ProjectCard({ project: {name, imgSrc, imgAlt, links} }) {
                 <Fragment key={name + ' ' + i}>
                   {
                     url ? (
-                      <a href={url}>
+                      <a href={url} target='_blank'>
                         <h2>{text}</h2>
                       </a>
                     ) : (
