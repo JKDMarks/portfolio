4f224c457410aefe3b00f9436aa4df9fef16caa2
Jeffrey Marks <JKDMarks@gmail.com>
2019-11-02 22:02:47 -0400

    * => {...} destructured properly

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import 'semantic-ui-css/semantic.min.css'
 import './App.css'
 
-import * from './containers'
+import { Summary, ProjectsContainer } from './containers'
 
 function App() {
   return (
